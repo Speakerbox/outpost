e7d52ab70905419378294dc74c80578d1eed4ae8
andrewholsted <andrew@fajitanachos.com>
2015-12-17 21:09:18 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Outpost
+
+Style guide - [https://github.com/felixge/node-style-guide](https://github.com/felixge/node-style-guide).  
+
---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Load our config before anything else
+require('./config');
+
+let server = require('./server');
+server.init();
