a37824547e759393ca89ac28fbea467e05530b4c
andrewholsted <andrew@fajitanachos.com>
2015-12-19 20:04:02 -0700

    Add basic websocket connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 'use strict';
 
+let async = require('async');
+let socket = require('./socket');
 
+module.exports = {
+  init: init
+}
 
-  
-
-
-
-
-
+function init(done){
+  var tasks = [
+    socket.init
+  ];
+  async.series(tasks, done);
+};
