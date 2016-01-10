0ce992da49714759cdfcf6975b939af9a4611704
andrewholsted <andrew@fajitanachos.com>
2016-01-10 09:11:15 -0700

    Rearrange socket events

--- server/socket/index.js
+++ server/socket/index.js
@@ -4,7 +4,6 @@ let Speaker = require('speaker');
 let ioClient = require('socket.io-client');
 let ioStream = require('socket.io-stream');
 let mic = require('microphone');
-let stream = ioStream.createStream();
 let nconf = require('nconf');
 let token = nconf.get('TOKEN');
 let socketUrl = nconf.get('socket:url');
@@ -37,21 +36,25 @@ function connect(done){
     console.log(err);
   });
 
-  socket.on('connect', function(socket, err) {
+  socket.on('connect', function() {
     console.log('Socket is connected to ' + socketUrl);
+
+    let stream = ioStream.createStream();
+
+    mic.startCapture();
+    mic.audioStream.pipe(stream);
+
+    ioStream(socket).on('receiveAudio', function(stream){
+      stream.pipe(speaker);
+    });
+
+    ioStream(socket).emit('streamAudio', stream);
   });
 
   socket.on('disconnect', function(socket) {
     console.log('Socket has been disconnected');
   });
 
-  ioStream(socket).emit('streamAudio', stream);
-  ioStream(socket).on('receiveAudio', function(stream){
-    stream.pipe(speaker);
-  });
-
-  mic.startCapture();
-  mic.audioStream.pipe(stream);
 };
 
 function disconnect(done){
